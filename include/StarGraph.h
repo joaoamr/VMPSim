0b1faa309a06d5c56d08e026dc7acba277f3b99a
João Magri <joaoa.mr@hotmail.com>
2019-03-25 20:01:54 -0300

    Add files via upload

---
+++ include/StarGraph.h
@@ -0,0 +1,18 @@
+#include <Graph.h>
+
+#ifndef STARGRAPH_H
+#define STARGRAPH_H
+
+class StarGraph : public Graph{
+    private:
+        int k;
+        int n;
+
+    public:
+        double getEdge(Vertex* vi, Vertex* vj) override;
+        double getEdge(int vi, int vj) override;
+        StarGraph(int ports, int size);
+
+};
+
+#endif
